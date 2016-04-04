6f4015006e30eb30c528f5ac3e415eb673b57c2e
t.ohmomo <donaldduck4369@yahoo.co.jp>
2016-04-04 10:43:26 +0900

    comment add

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Hello,rails"
+    render text: "Hello,rails app test"
   end
 end
